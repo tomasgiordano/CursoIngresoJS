fb604ef3271b6cbf412d7f32b82fda75b8c4d7e0
alumno <alumno>
2019-02-13 12:12:43 -0300

    Termine el ej Adivina el numero 2.0

--- 3-InstruccionSwitch/jsInstruccionSwitch(1).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(1).js
@@ -3,6 +3,19 @@ function mostrar()
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
 
+switch(mesDelAño){
+
+case "Enero":
+			alert("Que comiences bien el año!!!");
+			break;
+case "Julio":
+			alert("Se vienen las vacaciones!!!");
+			break;
+case "Diciembre":
+			alert("Felices fiestas!!");
+			break;
+
+}
 
 
 
