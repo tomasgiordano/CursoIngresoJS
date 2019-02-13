905b38387276785d68630ed6ed87a1ef664b9558
alumno <alumno>
2019-02-13 10:56:37 -0300

    Termine ej 3 de parcial

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/dos.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/dos.js
@@ -1,4 +1,18 @@
 function mostrar()
 {
-  
+  var recargo;
+  var precioEfectivo;
+  var precioFinal;
+
+  precioEfectivo=prompt("Ingrese el precio del producto: ","$$$");
+  precioEfectivo=parseInt(precioEfectivo);
+
+  recargo=(precioEfectivo*10)/100;
+  recargo=parseInt(recargo);
+  precioFinal=parseInt(precioFinal);
+  precioFinal=recargo+precioEfectivo;
+
+  elNombre.value=("Precio en efectivo: "+precioEfectivo);
+  laLocalidad.value=("Precio con tarjeta: "+precioFinal);
+
 }
