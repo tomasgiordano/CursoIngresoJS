57a31ecdb4921ac61ed5b37dc2a7e9edfcdef52e
alumno <alumno>
2019-02-13 09:35:38 -0300

    Termine el 2 de parcial

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,20 @@
 
 function mostrar()
 {
+var persona1;
+	var persona2;
+	var edad1;
+	var edad2;
+	var sumaEdades;
 
+	persona1=prompt("Escriba el nombre de la primera persona: ");
+	edad1=prompt("Escriba la edad de "+persona1);
+	edad1=parseInt(edad1);
+	persona2=prompt("Escriba el nombre de la segunda persona:  ");
+	edad2=prompt("Escrba la edad de "+persona2);
+	edad2=parseInt(edad2);
+
+	sumaEdades=edad1+edad2;
+
+	alert("Ustedes son "+persona1+" y "+persona2+" sus edades son "+edad1+" y "+edad2+", y la suma de sus edades es: "+sumaEdades);
 }
