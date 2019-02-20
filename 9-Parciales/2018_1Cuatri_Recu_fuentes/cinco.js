7ca58d1f3bfa89afcc79dc737fc8b83b2560db5a
tomasgiordano <tomiigiordano@gmail.com>
2019-02-20 11:54:08 -0300

    RECUPERATORIOS 4 Y 5

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cinco.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cinco.js
@@ -1,4 +1,77 @@
 function mostrar()
 {
+	/*
+													SWITCH:
+
+		Cada habitacion de un hotel tiene precio , hay promociones segun el tipo de pago si es con tarjeta visa
+	un 10%, si es por paypal un 15%, por mercadoPago un 10%, efectivo 20%, cualquier otro medio un 5%.
+		Si compraste el paquete "todoIncluido" y pagas con paypal se te suman 10% al descuento.
+		Si pagas en efectivo, tenes varias opciones, el paquete "soloDesayunos" te suma un 10% al descuento,
+	si el paquete es "todoIncluido" te suma un 15% y para el resto de los paquete no tiene descuento adicional.
+
+	*/
+	var mensaje;
+	var precioHabitacion;
+	var formaDePago;
+	var paquete;
+	var total;
+	var descuento;
+	descuento=parseInt(descuento);
+
+	precioHabitacion=prompt("Ingrese el monto de la habitacion: ","$$$");
+	precioHabitacion=parseInt(precioHabitacion);
+	mensaje="Su habitacion cuesta $"+precioHabitacion;
+
+
+	formaDePago=prompt("Cual va a ser su forma de pago?","visa/paypal/mercadoPago/efectivo/otro");
+	mensaje=mensaje+", su forma de pago es: "+formaDePago;
+
+	paquete=prompt("Que paquete quiere agregar a su compra?","todoIncluido/soloDesayunos/otro");
+	mensaje=mensaje+", y llevo el paquete: "+paquete; 
+
+
+	switch (formaDePago){
+		case "visa":
+		case "mercadoPago":
+			descuento=10;
+			mensaje=mensaje+". Por su forma de pago tuvo un "+descuento+"% de descuento.";
+			break;
+		case "paypal":
+			descuento=15;
+				switch(paquete){
+					case "todoIncluido":
+						descuento=descuento+10;
+						mensaje=mensaje+". Por su forma de pago y su paquete tuvo un "+descuento+"% de descuento.";
+						break;
+					default:
+					mensaje=mensaje+". Por su forma de pago tuvo un "+descuento+"% de descuento.";
+						break;
+				}//Cierra SWITCH (paquete) paypal
+			break;
+		case "efectivo":
+			descuento=20;			
+				switch(paquete){
+					case "todoIncluido":
+						descuento=descuento+15;
+						mensaje=mensaje+". Por su forma de pago y su paquete tuvo un "+descuento+"% de descuento.";
+						break;
+					case "soloDesayunos":
+						descuento=descuento+10;
+						mensaje=mensaje+". Por su forma de pago y su paquete tuvo un "+descuento+"% de descuento.";
+						break;
+					default:
+						mensaje=mensaje+". Por su forma de pago tuvo un "+descuento+"% de descuento.";
+						break;
+				}//Cierra SWITCH (paquete) efectivo
+			break;
+		default:
+			descuento=5;
+			mensaje=mensaje+". Por su forma de pago tuvo un "+descuento+"% de descuento.";
+			break;
+	}//Cierra SWITCH (formaDePago)
+
+total=((precioHabitacion*descuento)/100)+precioHabitacion;
+mensaje=mensaje+" El total a pagar es: "+total+".";
+alert(mensaje);
 
 }
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
@@ -1,4 +1,113 @@
 function mostrar()
 {
+/*Sistema que me ingrese 4 importes, mostrar el mayor,
+despues decir cuanto es el total, si supera 100$ hacer un descuento
+del 10%, si supera los 50$ el descuento es del 5%, en el caso de ser
+menos de 50$, hacerle un recargo del 15% */
+
+//INICIALIZO VARIABLES
+
+var valor1;
+var valor2;
+var valor3;
+var valor4;
+var total;
+var totalFinal;
+var mensaje;
+var maximo;
+
+//PIDO LOS VALORES DE LOS IMPORTES
+
+valor1=prompt("Ingrese el monto del PRIMER importe: ","$$$");
+valor2=prompt("Ingrese el monto del SEGUNDO importe: ","$$$");
+valor3=prompt("Ingrese el monto del TERCER importe: ","$$$");
+valor4=prompt("Ingrese el monto del CUARTO importe: ","$$$");
+
+//PARSEO A INTEGER
+
+valor1=parseInt(valor1);
+valor2=parseInt(valor2);
+valor3=parseInt(valor3);
+valor4=parseInt(valor4);
+
+//COMPARACION PARA SACAR EL MAYOR
+
+/*
+if(valor1>=valor2){//CONDICION DE VALOR 1
+	if(valor1>=valor3){
+		if(valor1>=valor4){
+			mensaje="El monto mayor es $"+valor1;
+		}else{//ELSE DE VALOR 4
+			if(valor4>=valor2){
+				if(valor4>=valor3){
+					mensaje="El monto mayor es $"+valor4;
+				}
+			}
+		}
+	}else{//ELSE DE VALOR 3
+		if(valor3>=valor2){
+			if(valor3>=valor4){
+				mensaje="El monto mayor es $"+valor3;
+			}
+		}
+	}
+}else{//ELSE DE VALOR 2
+	if(valor2>=valor3){
+		if(valor2>=valor4){
+			mensaje="El monto mayor es $"+valor2;
+		}
+	}
+}
+*/
+
+/*
+OTRA OPCION MAS EFICIENTE Y EFICAZ
+
+maximo=Math.Max(valor1,valor2,valor3,valor4);
+mensaje="El monto mayor es $ "+maximo;
+
+*/
+
+if(valor1>=valor2 && valor1>=valor3 && valor1>=valor4 ){
+	mensaje="El monto mayor es $"+valor1;
+
+}else{
+
+	if(valor2>=valor1 && valor2>=valor3 && valor2>=valor4){
+	mensaje="El monto mayor es $"+valor2;
+
+	}else{
+
+		if(valor3>=valor1 && valor3>=valor2 && valor3>=valor4){
+			mensaje="El monto mayor es $"+valor3;
+
+		}else{
+			mensaje="El monto mayor es $"+ valor4;
+
+		}
+	}
+}
+
+
+
+//SACAMOS CUANTO VALE EL TOTAL
+
+total=valor1+valor2+valor3+valor4;
+mensaje=mensaje+", el total de su compra fue $"+total;
+
+//CALCULAMOS CUANTO DESCUENTO TIENE
+
+if(total>=100){
+	totalFinal=total-((total*10)/100);
+	mensaje=mensaje+", como su compra supero o igualo los $100, tuvo un descuento del 10%, siendo el precio final $"+totalFinal;	
+}else if(total>=50){
+	totalFinal=total-((total*5)/100);
+	mensaje=mensaje+", como su compra supero o igualo los $50, tuvo un descuento del 5%, siendo el precio final $"+totalFinal;
+}else{
+	totalFinal=((total*15)/100)+total;
+	mensaje=mensaje+", por el valor de su compra, usted tiene un recargo del 15%, el precio final es $"+totalFinal;	
+}
+
+alert(mensaje);
 
 }
