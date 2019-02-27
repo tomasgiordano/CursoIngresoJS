6783a8ec9c0cc47cb50ad6de4063e93a77780c1a
tomasgiordano <tomiigiordano@gmail.com>
2019-02-27 10:58:12 -0300

    Ej 7 recu

--- 5-InstruccionWhile/jsIteraciones (10).js
+++ 5-InstruccionWhile/jsIteraciones (10).js
@@ -4,12 +4,33 @@ function mostrar()
 	var contador=0;
 	//declarar contadores y variables 
 	
-	var respuesta="si";
+	//var respuesta="si";
+	var respuesta;
+	var ingrese;
+	respuesta = true;
 
-	while(respuesta!="no")
-	{
+	while(respuesta==true)
+	{	
+		ingrese=prompt("Ingrese un numero:");
+		ingrese=parseInt(ingrese);
+		while(isNaN(ingrese)==true){
+			alert("No es un numero.");
+			break;
+		}
+
+		contador=contador+1;
+
+		respuesta=confirm("Desea continuar ingresando numeros?");
+
+		if(respuesta!=true)
+		{
+			alert("Usted dejó de ingresar numeros, procesando respuesta...")
+			break;
+		}
 		
-	
+
+
+
 	}
 
 
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/siete.js
@@ -1,4 +1,52 @@
 function mostrar()
 {
+	var nota;
+	var sexo;
+	var mNotaAprobada=0;
+	var mensaje;
+	var sexoNotaMasBaja;
+	var contador=0;
+	var sumaNotas=0;
+	var notaMenor=100;
+	var promedio;
+
+	while(contador<5)
+	{
+		sexo=prompt("Ingrese el sexo del alumno:","f o m");
+		while(sexo!="m" && sexo!="f")
+		{
+			sexo=prompt("Error, ingrese un sexo valido por favor.","f o m");
+		}
+		nota=prompt("Ingrese la nota del alumno:");
+		while(isNaN(nota)==true || (nota<0 || nota>10))
+		{
+			nota=prompt("Error, ingrese una nota valida:");
+		}
+
+		//PARA EL PROMEDIO
+		nota=parseInt(nota);
+		sumaNotas=sumaNotas+nota;
+
+		mensaje="El promedio de todas las notas es ";
+
+		//NOTA MENOR Y SU SEXO
+		if(nota<notaMenor)
+		{
+			notaMenor=nota;
+			sexoNotaMasBaja=sexo;
+		}
+	
+		//CANTIDAD DE VARONES APROBADOS
+		if(sexo="m"&&nota>=6)
+		{
+			mNotaAprobada=mNotaAprobada+1;
+		}
+
+		contador=contador+1;
+	}
+
+	promedio=sumaNotas/5;
+	mensaje=mensaje+promedio+", la nota mas baja fue "+notaMenor+" y su sexo es "+sexoNotaMasBaja+". Aprobaron "+mNotaAprobada+" varones de los que rindieron."
+	alert(mensaje);
 
 }
